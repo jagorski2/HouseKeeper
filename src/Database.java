2025093c4a28020b93cf0b25ead07b76d6318692
Jim Gorski <jgorski2@buffalo.edu>
2016-11-07 16:07:41 -0500

    Syncing

--- src/Database.java
+++ src/Database.java
@@ -9,6 +9,7 @@ public class Database {
 	private MysqlDataSource dataSource;
 	private Connection conn;
 	private Statement stmt;
+
 	public Database(String username, String password, String server) {
 		dataSource = new MysqlDataSource();
 		dataSource.setUser(username);
@@ -33,25 +34,22 @@ public class Database {
 		conn.close();
 
 	}
-	
+
 	public String[] getHouses(String user) throws SQLException {
 		ResultSet resret;
 		String[] houses;
 		int count = 0;
 		conn = (Connection) dataSource.getConnection();
 		stmt = (Statement) conn.createStatement();
-		resret = stmt.executeQuery("SELECT count(*) AS count FROM `"+user+"_HOUSES");
+		resret = stmt.executeQuery("SELECT count(*) AS count FROM `" + user + "_HOUSES");
 		resret.first();
 		houses = new String[Integer.parseInt(resret.getString("count"))];
-		resret = stmt.executeQuery("SELECT * FROM `"+user+"_HOUSES");
+		resret = stmt.executeQuery("SELECT * FROM `" + user + "_HOUSES");
 
-
-		while (resret.next())
-		{	
+		while (resret.next()) {
 			houses[count++] = resret.getString("myHouses");
 		}
-		
-		
+
 		resret.close();
 		stmt.close();
 		conn.close();
@@ -64,23 +62,21 @@ public class Database {
 		conn = (Connection) dataSource.getConnection();
 		stmt = (Statement) conn.createStatement();
 		String[] saltPW = new String[2];
-		ResultSet qresult = stmt.executeQuery("SELECT * from `users` WHERE userName like '"+uname+"'");
-		
-		if (qresult.first())
-		{
+		ResultSet qresult = stmt.executeQuery("SELECT * from `users` WHERE userName like '" + uname + "'");
+
+		if (qresult.first()) {
 
 			returnValue = false;
-		}
-		else
-		{
-			
+		} else {
+
 			saltPW = Crypto.hashPW(pass);
 			String q = "INSERT INTO `users` (`userID`, `userName`, `pass`, `salt`) VALUES (NULL, '" + uname + "', '"
 					+ saltPW[1] + "', '" + saltPW[0] + "');";
 			stmt.executeUpdate(q);
-			q = "CREATE TABLE `housekeeper`.`"+ uname.trim() + "_HOUSES` ( `myHouses` VARCHAR(100) NOT NULL ) ENGINE = InnoDB";
+			q = "CREATE TABLE `housekeeper`.`" + uname.trim()
+					+ "_HOUSES` ( `myHouses` VARCHAR(100) NOT NULL ) ENGINE = InnoDB";
 			stmt.executeUpdate(q);
-			
+
 			returnValue = true;
 		}
 
@@ -89,18 +85,18 @@ public class Database {
 		return returnValue;
 
 	}
-	
+
 	public boolean addHouse(String uname, String houseName) throws SQLException {
 		boolean returnValue;
 		conn = (Connection) dataSource.getConnection();
 		stmt = (Statement) conn.createStatement();
-		
-		
-		String q = "INSERT INTO `houses` (`houseID`, `houseName`, `houseOwner`) VALUES (NULL, '"+houseName.trim() + "_HOUSE', '"+uname+"');";
-			stmt.executeUpdate(q);
-	   q = "INSERT INTO `"+uname+"_houses` (`myHouses`) VALUES ('"+houseName+"')";
-	   stmt.executeUpdate(q);
-			returnValue = true;
+
+		String q = "INSERT INTO `houses` (`houseID`, `houseName`, `houseOwner`) VALUES (NULL, '" + houseName.trim()
+				+ "_HOUSE', '" + uname + "');";
+		stmt.executeUpdate(q);
+		q = "INSERT INTO `" + uname + "_houses` (`myHouses`) VALUES ('" + houseName + "')";
+		stmt.executeUpdate(q);
+		returnValue = true;
 
 		stmt.close();
 		conn.close();
@@ -115,16 +111,14 @@ public class Database {
 		conn = (Connection) dataSource.getConnection();
 		stmt = (Statement) conn.createStatement();
 		resret = stmt.executeQuery(q);
-		if(resret.next()) {
-		ret[0] = resret.getString("salt");
-		ret[1] = resret.getString("pass");
+		if (resret.next()) {
+			ret[0] = resret.getString("salt");
+			ret[1] = resret.getString("pass");
 		}
 		resret.close();
 		stmt.close();
 		conn.close();
 		return ret;
 	}
-	
-	
 
 }
